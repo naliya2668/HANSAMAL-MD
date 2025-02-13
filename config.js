387211764a9c9a88a4f6d0fdf14504977f2bbd54
naliya2668 <116560234+naliya2668@users.noreply.github.com>
2025-02-13 12:00:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? 'HANSAMAL-MD=V1wRwCrC#ytkzbSndfnI7gHE5Z5gp63Bbkq0KDv5xYsHLeXUUBww' : process.env.SESSION_ID,
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN,        
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'naliya2668' : process.env.GITHUB_USER_NAME,
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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' : process.env.GITHUB_AUTH_TOKEN,        
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94711262551' : process.env.OWNER_NUMBER
 };
